ac50c092e4c58fc783345aeef641881cf40ddeea
viktor <vikror@seznam.cz>
2024-10-28 20:20:29 +0100

    Part 10 - render a texture

--- common/src/main/java/com/bura/common/model/Shader.kt
+++ common/src/main/java/com/bura/common/model/Shader.kt
@@ -5,12 +5,22 @@ import com.bura.common.util.Constants
 
 class Shader(
     colorProgram: Int,
+    textureProgram: Int,
 ) {
     val color = Color(
         program = colorProgram,
         aPositionHandle = Engine.gles20.glGetAttribLocation(colorProgram, Constants.A_POSITION),
         uMatrixHandle = Engine.gles20.glGetUniformLocation(colorProgram, Constants.U_MATRIX),
         uColorHandle = Engine.gles20.glGetUniformLocation(colorProgram, Constants.U_COLOR),
+    )
+
+    val texture = Texture(
+        program = textureProgram,
+        aPositionHandle = Engine.gles20.glGetAttribLocation(textureProgram, Constants.A_POSITION),
+        uMatrixHandle = Engine.gles20.glGetUniformLocation(textureProgram, Constants.U_MATRIX),
+        uColorHandle = Engine.gles20.glGetUniformLocation(textureProgram, Constants.U_COLOR),
+        uTextureHandle = Engine.gles20.glGetUniformLocation(textureProgram, Constants.U_TEXTURE),
+        aTextureHandle = Engine.gles20.glGetAttribLocation(textureProgram, Constants.A_TEXTURE),
     )
 
     interface Type {
@@ -20,6 +30,15 @@ class Shader(
     data class Color(
         override var program: Int,
         var aPositionHandle: Int,
+        var uMatrixHandle: Int,
+        var uColorHandle: Int,
+    ): Type
+
+    data class Texture(
+        override var program: Int,
+        var uTextureHandle: Int,
+        var aPositionHandle: Int,
+        var aTextureHandle: Int,
         var uMatrixHandle: Int,
         var uColorHandle: Int,
     ): Type
